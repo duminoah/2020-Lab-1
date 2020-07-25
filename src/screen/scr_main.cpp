8e5e829a8296e81c081a1261a246ba32fa8b9067
francmeister <francophiri97@gmail.com>
2020-07-25 18:47:11 +0200

    Exercise 4.2

--- src/screen/scr_main.cpp
+++ src/screen/scr_main.cpp
@@ -27,6 +27,7 @@ int main()
 	myScreen.set("SCREEN");
 	myScreen.display();
 
+    //Exercise 4.1
 	//Making the screen to display my initial 'F'
 	myScreen.clear(' ');
 	myScreen.home();
@@ -49,7 +50,19 @@ int main()
 	myScreen.down();
 	myScreen.display();
 
+    /*Exercise 4.2
+        1. The const keyword is being used at the end of member functions - string::size_type height() const { return height_; }
+           This specifies that the member function height cannot modify any member variable of the class type screen.
+        2. The const keyword is being used in member function definitions to specify formal parameters as constant reference
+           parameters - void set( const string& s );
+           This ensures that in a call to the function, the formal parameter receives the address of the actual parameter, but the
+           formal parameter cannot modify the value of the actual parameter.
+        3. The const keyword is being used to declare member variables as constant variables - const string::size_type TOP_LEFT = 0;
+           This specifies that the value of the constant variable cannot be modified at any point in the execution of the program.
 
+        The string class used at member function (string::size_type) allows for efficient use of memory by holding the size of
+        a string nomatter how big it is. The size of the variable is not fixed, it is dictated by the string itself.
+    */
 
 	return 0;
 }
